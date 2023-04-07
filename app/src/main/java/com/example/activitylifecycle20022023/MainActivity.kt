8a32861a516d486e13b5c37c24559c0c8ca72d89
Phat <phatdroid94@gmail.com>
2023-04-07 20:07:27 +0700

    screen 3

--- app/src/main/java/com/example/activitylifecycle20022023/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycle20022023/MainActivity.kt
@@ -1,43 +1,54 @@
 package com.example.activitylifecycle20022023
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnScreen2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("BBB", "onCreate")
+        Log.d("BBB", "Main1: onCreate")
+
+        btnScreen2 = findViewById(R.id.button_navigate_screen_2)
+
+        btnScreen2.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("BBB", "onStart")
+        Log.d("BBB", "Main1: onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("BBB", "onResume")
+        Log.d("BBB", "Main1: onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("BBB", "onPause")
+        Log.d("BBB", "Main1: onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("BBB", "onStop")
+        Log.d("BBB", "Main1: onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("BBB", "onDestroy")
+        Log.d("BBB", "Main1: onDestroy")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("BBB", "onRestart")
+        Log.d("BBB", "Main1: onRestart")
     }
 }
--- app/src/main/java/com/example/activitylifecycle20022023/MainActivity2.kt
+++ app/src/main/java/com/example/activitylifecycle20022023/MainActivity2.kt
@@ -1,11 +1,54 @@
 package com.example.activitylifecycle20022023
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var btnNavigateScreen3: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        Log.d("BBB", "Main2: onCreate")
+
+        btnNavigateScreen3 = findViewById(R.id.button_navigate_screen_3)
+        btnNavigateScreen3.setOnClickListener {
+            val intent = Intent(this@MainActivity2, MainActivity3::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("BBB", "Main2: onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("BBB", "Main2: onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("BBB", "Main2: onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("BBB", "Main2: onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("BBB", "Main2: onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("BBB", "Main2: onRestart")
     }
 }
